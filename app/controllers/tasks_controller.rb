f72c8a14847b3a4443b663d24867504d926c15c9
kei.mori <mrtk1@icloud.com>
2019-06-10 12:32:27 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy, :update]
+  before_action :correct_user, only: [:show, :edit, :destroy, :update]
   
   def index
      if logged_in?
@@ -10,19 +10,18 @@ class TasksController < ApplicationController
   end
   
   def show
-    @tasks = current_user.tasks.find(params[:id])
   end
   
   def new
-    @tasks = Task.new
+    @task = Task.new
   end
   
   def create
-    @tasks = current_user.tasks.build(task_params)
+    @task = current_user.tasks.build(task_params)
     
-    if @tasks.save
+    if @task.save
       flash[:success] = 'Task が正常に作成されました'
-      redirect_to @tasks
+      redirect_to @task
     else
       flash.now[:danger] = 'Task が作成できませんせした'
       render :new
@@ -30,15 +29,14 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @tasks = Task.find(params[:id])
   end
   
   def update
-    @tasks = Task.find(params[:id])
+    @task = Task.find(params[:id])
     
-    if @tasks.update(task_params)
+    if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
-      redirect_to @tasks
+      redirect_to @task
     else
       flash.now[:danger] = 'Task は正常に更新されませんでした'
       render :edit
@@ -46,8 +44,7 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @tasks = Task.find(params[:id])
-    @tasks.destroy
+    @task.destroy
     
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
